803628f0df65f333f07895047a48e44fbe6d002d
Pyrojet99 <giacomo.cignoni@gmail.com>
2016-12-03 18:17:31 +0100

    modified imgsnames in index

--- featuresList.js
+++ featuresList.js
@@ -3,7 +3,7 @@ app.controller('featuresController', function () {
     features = [
         {
             name: '3D Acquisition: Aligning ',
-            image: 'img/Feature/Alignment.png',
+            image: 'img/Feature/Alignment.jpeg',
             description: 'The 3D data alignment phase (also known as registration) is a fundamental step in the pipeline for processing 3D scanned data. MeshLab provides a powerful tool for moving the different meshes into a common reference system, able to manage large set of range-maps. MeshLab implements a fine tuned ICP one-to-one alignment step, followed by a global bundle adjustment error-distribution step. The alignment can be performed on meshes and point clouds coming from several sources, including active (both short- and long-range) scanners and 3D-from-image tools.'
                             },
         {
@@ -13,12 +13,12 @@ app.controller('featuresController', function () {
                             },
         {
             name: '3D Acquisition: Color Mapping and Texturing ',
-            image: 'img/Feature/Texturing.png',
+            image: 'img/Feature/Texturing.jpeg',
             description: 'Color information may be as important as geometry, but several acquisition technologies do not provide accurate appearance data. MeshLab contains a pipeline for the alignment and projection of color information (from a set of uncalibrated images) onto a 3D model. Several automatic and assisted methods are provided to obtain a high quality color encoding, with both per-vertex or texture mapping.'
                             },
         {
             name: 'Cleaning 3D Models',
-            image: 'img/Feature/Cleaning.png',
+            image: 'img/Feature/Cleaning.jpeg',
             description: 'MeshLab offers a series of automatic, semi-manual and interactive filters to remove those geometric element generally considered “wrong” by most software and algorithms. It is possible to removing topological errors, duplicated and unreferenced vertices, small components, degenerated or intersecting faces, and many more geometrical and topological singularities. Using different automatic and interactive selection methods, is then possible to isolate and remove unwanted areas of your meshes and point clouds.'
                             },
         {
@@ -33,7 +33,7 @@ app.controller('featuresController', function () {
                             },
         {
             name: 'Measurement, and Analysis',
-            image: 'img/Feature/Simplification.png',
+            image: 'img/Feature/Simplification.jpeg',
             description: 'Interactive point-to-point measurement of a 3D model is really easy in MeshLab. Moreover, automatic filters will return various geometric and topological information about your 3D model (or just of a selected area), while the Sectioning tool can export cut-through sections of a mesh as polylines. Different geometric information (like curvature, geodesic distance, or local vertex density) may be calculated on meshes and 3D models using automatic filters.'
                             },
         {
@@ -53,7 +53,7 @@ app.controller('featuresController', function () {
                             },
         {
             name: 'Comparing Models',
-            image: 'img/Feature/Comparing.jpg',
+            image: 'img/Feature/Comparing.jpeg',
             description: 'Measuring the geometric difference between two 3D models using Hausdorff Distance is a common approach in mesh processing. Many years ago (in 1997!), the Visual Computing Lab developed and freely distributed what become the standard tool for such task, Metro; the related paper has been cited more than one thousand times, thus demonstrating the impact of that work. While the original Metro tool was still a small open source standalone command line program (still available at our web site), MeshLab offers now much more advanced and versatile functionalities for comparing two meshes, that also compute signed distance and may work on point clouds.'
                             },
         {
@@ -63,7 +63,7 @@ app.controller('featuresController', function () {
                             },
         {
             name: 'Raster Layers: integration with images',
-            image: 'img/Feature/raster.png',
+            image: 'img/Feature/raster.jpeg',
             description: 'Raster Layers have been introduced in MeshLab to allow to go beyond the standard 3D model. MeshLab users can import in a project also images and other 2D entities. These Raster Layers can be used not only to project color information on the 3D model, but also to generate peculiar points of view, or store an entire 3d-from-image acquisition procedure by including also the images used for the generation of the final results.'
                             }
 
